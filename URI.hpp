7922aacc64dbb09a0093e79980a2e6097edc0dd4
misanthropique <80142735+misanthropique@users.noreply.github.com>
2022-05-11 00:29:27 -0500

    Get and assignment methods

--- URI.hpp
+++ URI.hpp
@@ -72,4 +72,65 @@ public:
 	 */
 	URI(
 		const std::string& uriString );
+
+	/**
+	 * Get the scheme for the URI. For relative URIs, this is not defined.
+	 * @return Const reference to the scheme component of the URI.
+	 */
+	const std::string& getScheme() const;
+
+	/**
+	 * Get the user information. This information may or may not be present.
+	 * @return Const reference to the user information component of the URI.
+	 */
+	const std::string& getUserInformation() const;
+
+	/**
+	 * Get the host. If the URI comprises an authority, then this
+	 * component is required to be present.
+	 * @return Const reference to the host component of the URI.
+	 */
+	const std::string& getHost() const;
+
+	/**
+	 * Get the port. If the URI comprises an authority, a scheme is present, and
+	 * the port is not otherwise set, then if the scheme is present in the table of
+	 * known scheme default port numbers, then the default port is returned.
+	 * @return Const reference to the port component of the URI.
+	 */
+	const std::string& getPort() const;
+
+	/**
+	 * Get the path component of the URI.
+	 * @return Const reference to the path component of the URI.
+	 */
+	const std::string& getPath() const;
+
+	/**
+	 * Get the query component of the URI.
+	 * @return Const reference to the query component of the URI.
+	 */
+	const std::string& getQuery() const;
+
+	/**
+	 * Get the fragment component of the URI.
+	 * @return Const reference to the fragment component of the URI.
+	 */
+	const std::string& getFragment() const;
+
+	/**
+	 * Move assignment operator.
+	 * @param other R-Value to the URI object to move to this instance.
+	 * @return Reference to this URI instance is returned.
+	 */
+	URI& operator=(
+		URI&& other );
+
+	/**
+	 * Copy assignment operator.
+	 * @param other Const reference to the URI object to copy to this instance.
+	 * @return Reference to this URI instance is returned.
+	 */
+	URI& operator=(
+		const URI& other );
 };
